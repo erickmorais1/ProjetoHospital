8f5720b43d9cd40d8bc173eebcd02fb8c820fa06
erickmorais1 <erick.carvalho@alunos.ufersa.edu.br>
2026-04-16 18:36:41 -0300

    Update Paciente.java
    implementação da classe Paciente e de seus métodos

--- src/br/edu/ufersa/ProjetoHospital/model/entities/Paciente.java
+++ src/br/edu/ufersa/ProjetoHospital/model/entities/Paciente.java
@@ -1,4 +1,61 @@
 package br.edu.ufersa.ProjetoHospital.model.entities;
 
 public class Paciente {
+  private String nome,endereco,cpf;
+  private Prontuario prontuario;
+
+  public Paciente (){}
+  public Paciente (String nome, String endereco, String cpf){
+    setNome(nome);
+    setEndereco(endereco);
+    setCpf(cpf);
+  }
+
+  //getters
+  public String getNome(){
+    return nome;
+  }
+  public String getEndereco(){
+    return endereco;
+  }
+  public String getCpf(){
+    return cpf;
+  }
+  public Prontuario getProntuario(){
+    return prontuario;
+  }
+
+  public void setNome(String nome){
+    if(nome!=null && !nome.isEmpty()){
+      this.nome = nome;
+    }
+  }
+  public void setEndereco (String endereco){
+    if(endereco!=null && !endereco.isEmpty()){
+      this.endereco = endereco;
+    }
+  }
+  public void setCpf(String cpf){
+    if(cpf!=null && cpf.length()==11){
+      this.cpf = cpf;
+    }
+  }
+
+  public void mudarEndereco(String local){
+    if(local!=null && !local.isEmpty()){
+      this.endereco=local;
+    }
+  }
+
+ public void atualizarProntuario(Prontuario prontuario){
+  if(prontuario != null &&
+     prontuario.getObservacoes() != null &&
+     !prontuario.getObservacoes().isEmpty() &&
+     prontuario.getData() != null){
+       
+    this.prontuario = prontuario;
+  }
+}
+
+  
 }
