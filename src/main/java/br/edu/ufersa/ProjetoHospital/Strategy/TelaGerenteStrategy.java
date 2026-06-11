7c04d9fe95c6bdefd080c3a5cdd40f28b7fabbd9
erick morais <erick.carvalho@alunos.ufersa.edu.br>
2026-06-11 10:27:23 -0300

    Criação da TelaStrategy usando o padrão de criação.

---
+++ src/main/java/br/edu/ufersa/ProjetoHospital/Strategy/TelaGerenteStrategy.java
@@ -0,0 +1,11 @@
+package br.edu.ufersa.ProjetoHospital.Strategy;
+
+import javafx.stage.Stage;
+
+public class TelaGerenteStrategy implements TelaStrategy {
+
+    @Override
+    public void carregarTela(Stage stage) {
+
+    }
+}
---
+++ src/main/java/br/edu/ufersa/ProjetoHospital/Strategy/TelaStrategy.java
@@ -0,0 +1,9 @@
+package br.edu.ufersa.ProjetoHospital.Strategy;
+
+import javafx.stage.Stage;
+
+public interface TelaStrategy {
+
+    void carregarTela(Stage stage);
+
+}
