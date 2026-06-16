07cba3d44039d38acd09d82a6616f5f3b9e5a6a2
erick morais <erick.carvalho@alunos.ufersa.edu.br>
2026-06-16 09:59:24 -0300

    Criação da tela inicial do medico

---
+++ src/main/java/br/edu/ufersa/ProjetoHospital/Controller/MenuMedicoController.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.ProjetoHospital.Controller;
+
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+
+public class MenuMedicoController {
+
+    @FXML
+    public void abrirCadastrarConsulta(javafx.event.ActionEvent actionEvent) {
+        System.out.println("Abrir tela de cadastrar consulta");
+    }
+}
