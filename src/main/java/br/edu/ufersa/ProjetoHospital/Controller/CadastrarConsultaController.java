266a767686dbc17bb6ee28d00584c29ec2bb3f29
erick morais <erick.carvalho@alunos.ufersa.edu.br>
2026-06-17 10:43:53 -0300

    Liguei a CadastrarConsultaController.java ao HospitalFacade.java

--- src/main/java/br/edu/ufersa/ProjetoHospital/Controller/CadastrarConsultaController.java
+++ src/main/java/br/edu/ufersa/ProjetoHospital/Controller/CadastrarConsultaController.java
@@ -1,14 +1,23 @@
 package br.edu.ufersa.ProjetoHospital.Controller;
 
+import br.edu.ufersa.ProjetoHospital.Facade.HospitalFacade;
+import br.edu.ufersa.ProjetoHospital.Service.PacienteService;
+import br.edu.ufersa.ProjetoHospital.model.entities.Endereco;
+import br.edu.ufersa.ProjetoHospital.model.entities.Medico;
+import br.edu.ufersa.ProjetoHospital.model.entities.Paciente;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
+
 public class CadastrarConsultaController {
 
     @FXML
     private DatePicker dpDataConsulta;
+
+    private HospitalFacade facade;
 
     @FXML
     private Button btnCadastrar;
@@ -20,7 +29,46 @@ public class CadastrarConsultaController {
     private TextField txtCpfPaciente;
 
     @FXML
-    private void cadastrarConsulta() {
-        System.out.println("Data escolhida: " + dpDataConsulta.getValue()) ;
+    private void cadastrarConsulta() throws PacienteService.ServicoException, SQLException {
+        Paciente paciente =
+                facade.buscarPacientePorCpf(
+                        txtCpfPaciente.getText()
+                );
+
+        System.out.println(
+                "Paciente encontrado: " +
+                        paciente.getNome()
+        );
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua do Médico");
+
+        Medico medico = new Medico(
+                "João Silva",
+                "11111111111",
+                endereco,
+                "CRM12345",
+                200.0
+        );
+
+        facade.agendarConsulta(
+                paciente,
+                medico,
+                dpDataConsulta.getValue()
+        );
+
+        System.out.println("Consulta cadastrada com sucesso!");
+
+        if (facade == null) {
+            System.out.println("Facade NULA");
+        } else {
+            System.out.println("Facade OK");
+        }
+
+        System.out.println("CPF: " + txtCpfPaciente.getText());
+        System.out.println("Data: " + dpDataConsulta.getValue());
+    }
+
+    public void setFacade(HospitalFacade facade) {
+        this.facade = facade;
     }
 }
