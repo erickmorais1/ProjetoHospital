bd571c852cf59ed71261d87c243636981dae6f6e
erick morais <erick.carvalho@alunos.ufersa.edu.br>
2026-06-18 19:22:47 -0300

    Criação da interface FacadeController para flexibilizar o codigo

---
+++ src/main/java/br/edu/ufersa/ProjetoHospital/Controller/FacadeController.java
@@ -0,0 +1,9 @@
+package br.edu.ufersa.ProjetoHospital.Controller;
+
+import br.edu.ufersa.ProjetoHospital.Facade.HospitalFacade;
+
+public interface FacadeController {
+
+    void setFacade(HospitalFacade facade);
+
+}
